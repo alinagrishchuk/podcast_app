cea5000afa3638c799d1a210fa4e5e81cf0e1da9
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-06-22 10:56:25 +0300

    add episodes controller & views

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,15 +1,20 @@
 class PodcastsController < ApplicationController
   before_action :find_podcast, only: [:show]
+  before_action :find_episode, only: [:show]
+
   def index
     @podcasts = Podcast.all
   end
   def show
-    @podcast
   end
 
   private
 
   def find_podcast
       @podcast = Podcast.find(params[:id])
+  end
+
+  def find_episode
+    @episodes = Episode.where(podcast_id: @podcast)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
-  resources :podcasts, only: [:index, :show ]
+  resources :podcasts, only: [:index, :show ] do
+    resources :episodes
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
