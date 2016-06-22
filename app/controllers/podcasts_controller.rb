a5979054d7879b9e86fa512ce4b579554b482365
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-06-22 11:26:25 +0300

    add dashboard for user

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,17 +1,26 @@
 class PodcastsController < ApplicationController
-  before_action :find_podcast, only: [:show]
-  before_action :find_episode, only: [:show]
+  before_action :find_podcast, only: [:show, :dashboard]
+  before_action :find_episode, only: [:show, :dashboard]
 
   def index
     @podcasts = Podcast.all
   end
+
   def show
+  end
+
+  def dashboard
+
   end
 
   private
 
   def find_podcast
-      @podcast = Podcast.find(params[:id])
+    @podcast = if params[:id].nil?
+                 current_podcast
+               else
+                 Podcast.find(params[:id])
+               end
   end
 
   def find_episode
