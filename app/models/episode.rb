0d6494543b05d775222d20859fd5a3e4d45a9098
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-06-22 16:39:25 +0300

    add mp3 upload to episodes

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -9,8 +9,20 @@ class Episode < ActiveRecord::Base
                                               user_id: ENV['USER_ID'],
                                               access_type: 'app_folder'},
                     :path => "episodes/:id/:style/:id_:filename"
-
   validates_attachment_content_type :episode_thumbnail, content_type: /\Aimage\/.*\Z/
+
+  has_attached_file     :mp3,
+                        :storage => :dropbox,
+                        :dropbox_credentials => { app_key: ENV['APP_KEY'],
+                                                  app_secret: ENV['APP_SECRET'],
+                                                  access_token: ENV['ACCESS_TOKEN'],
+                                                  access_token_secret: ENV['ACCESS_TOKEN_SECRET'],
+                                                  user_id: ENV['USER_ID'],
+                                                  access_type: 'app_folder'},
+                        :path => "episodes/mp3/:id/:id_:filename"
+  validates_attachment  :mp3,
+                        :content_type => { :content_type => ["audio/mpeg", "audio/mp3"] },
+                        :file_name => { :matches => [/mp3\Z/] }
 
   validates :podcast, presence: true
 
