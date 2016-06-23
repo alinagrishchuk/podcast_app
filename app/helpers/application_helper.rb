ae4dc48f504c07c026c0092718b8b79560b46f4c
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-06-23 14:11:46 +0300

    add bread_crumbs partial for link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+
+  def nav_link(text, link)
+    if current? link
+      active_link(text)
+    else
+      content_tag(:li) do
+        link(text, link)
+      end
+    end
+  end
+
+  private
+    # check - link equal current page
+    def current? link
+      request.env['PATH_INFO'] == link
+    end
+
+    # create link view element
+    def active_link(text)
+      content_tag(:li) do
+        content_tag :span, text, :class => 'active'
+      end
+    end
+
+    def link(text, link)
+      content_tag('li') do
+        link_to text, link
+      end
+    end
+
 end
