a39335384e46d03d4c0e9b3433a702781e087272
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-06-24 13:56:36 +0300

    apply custom file_field

--- app/helpers/custom_form_builder.rb
+++ app/helpers/custom_form_builder.rb
@@ -2,7 +2,7 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
   include ActionView::Helpers::TagHelper
   include ActionView::Context
 
-  def file_field_button (method, title,options={})
+  def file_field_button (method, title='select file',options={})
     content_tag :div, class: 'input-btn-wrapper' do
       content_tag(:span, title) +
       file_field(method, options)
